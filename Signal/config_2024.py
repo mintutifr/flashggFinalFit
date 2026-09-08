5234852b5d9291c75119a0fd5c69be3456cbdf15
mikumar <mintu.kumar@cern.ch>
2026-09-08 17:49:18 +0200

    update config_2024

--- Signal/config_2024.py
+++ Signal/config_2024.py
@@ -7,7 +7,7 @@ signalScriptCfg = {
   # Setup
   'inputWSDir':'/eos/user/m/mikumar/Final_fits/outputForFinalFits_7Sep2026/workspaces/%s/'%_year,
   'procs': 'auto', #'auto', # if auto: inferred automatically from filenames
-  'cats': 'tHq_had0_CP_mix', #'tHq_had0_CP_odd,tHq_had0_CP_even,ttH_had0_CP_even,ttH_had0_CP_odd,tHq_had0_CP_mix', #'auto', #'ttH_lep0_CP_even', #'auto', # if auto: inferred automatically from (0) workspace
+  'cats': 'tHq_had0_CP_odd,tHq_had0_CP_even,ttH_had0_CP_even,ttH_had0_CP_odd,tHq_had0_CP_mix', #'auto', #'ttH_lep0_CP_even', #'auto', # if auto: inferred automatically from (0) workspace
   'ext':'tth_th_cp_Analysis', #_%s'%_year,
   'analysis':'tth_th_cp_Analysis', # To specify which replacement dataset mapping (defined in ./python/replacementMap.py)
   'year':'%s'%_year, # Use 'combined' if merging all years: not recommended
